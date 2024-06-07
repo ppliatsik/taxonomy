833ee14fd6f3f486993652008fcfc9746b1739ae
ppliatsik <ppliatsik@gmail.com>
2024-06-07 17:32:47 +0300

    Add TLS support for nginx.

---
+++ scripts/create-nginx-certs.sh
@@ -0,0 +1,2 @@
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout certs/nginx-selfsigned.key -out certs/nginx-selfsigned.crt
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout certs/mail.key -out certs/mailcert.pem
