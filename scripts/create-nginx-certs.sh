6bcf4ea72a910427cc63459db168946689412e4e
ppliatsik <ppliatsik@gmail.com>
2024-11-27 20:42:14 +0200

    Fixes.

--- scripts/create-nginx-certs.sh
+++ scripts/create-nginx-certs.sh
@@ -1,2 +1,1 @@
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout certs/nginx-selfsigned.key -out certs/nginx-selfsigned.crt
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout certs/mail.key -out certs/mailcert.pem
